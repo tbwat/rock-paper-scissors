d115ffe2a28c22c665310c80d33197ef438f23b8
tbwat <twb11@proton.me>
2024-10-03 12:00:49 -0500

    change rps game to ui based from console based

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
 
-console.log("lets play!")
-
+const buttons = document.querySelectorAll("button");
+const totalScore = document.querySelector("#total-score p");
+const results = document.querySelector("#results p");
 
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-
-
 
 const getComputerChoice = () => {
     if (getRandomInt(1, 3) === 1) {
@@ -20,51 +18,56 @@ const getComputerChoice = () => {
     }
 }
 
-const getHumanChoice = () => {
-    console.log("")
-    let choice = prompt("choose rock, paper, or scissors!");
-    if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
-        return choice.toLowerCase();
-    } else {
-        console.log("wrong answer! try again")
-    }
-}
-
 const playRound = (humanChoice, computerChoice) => {
 
-    
     if ((humanChoice === "rock" && computerChoice === "scissors") || 
     (humanChoice === "paper" && computerChoice === "rock") || 
     (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log("You win!");
         humanScore++;
+        results.textContent = "You won this round!";
+        
     } else if ((humanChoice === "rock" && computerChoice === "paper") || 
     (humanChoice === "paper" && computerChoice === "scissors") || 
     (humanChoice === "scissors" && computerChoice === "rock")) {
-        console.log("You lose!");
         computerScore++;
+        results.textContent = "The computer wins this round!";
+        
     } else if (humanChoice === computerChoice) {
-        console.log("tie! try again!")
+        results.textContent = "tie! try again!";
     }
-
-    console.log(`you have ${humanScore} points`);
-    console.log(`computer has ${computerScore} points`);
-}
-
-const playGame = () => {
+    totalScore.textContent = `You: ${humanScore} points
+        Computer: ${computerScore} points`
     
-    for (let i = 1; i <= 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    console.log("that's the end of the Game!")
-    console.log(`final score:
-        you: ${humanScore}
-        computer: ${computerScore}`)
 }
+
+
 let humanScore = 0;
 let computerScore = 0;
 
 
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (humanScore < 5 && computerScore < 5) {
+            playRound(button.textContent, getComputerChoice())    
+        }
+        else if (humanScore === 5) {
+            results.textContent = "YOU WIN!"
+            totalScore.textContent = `Final Score:
+            
+            You {${humanScore} points} 
+            
+            Computer {${computerScore} points}`
+        }
+        else if (computerScore === 5) {
+            results.textContent = "you lose:("
+            totalScore.textContent = `Final Score: 
+            
+            You {${humanScore} points}
+            
+            Computer {${computerScore} points}`
+        }
+    })
+})   
+    
 
-playGame()
